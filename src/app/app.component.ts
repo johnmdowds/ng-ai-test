a81089a6cc9147d8417b6812ad6c6d8c7dd64eda
John Dowds <johnmdowds@googlemail.com>
2023-01-13 07:30:13 +0000

    add logic and html to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Configuration, OpenAIApi } from 'openai';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-ai-test';
+  openAiToken = environment.openAiToken;
+  openAiApi: OpenAIApi;
+  text: string = '';
+  response: any;
+
+  constructor() {
+    let configuration = new Configuration({
+      apiKey: environment.openAiToken
+    });
+    this.openAiApi = new OpenAIApi(configuration);
+  }
+
+  async doSomething(text: string) {
+    try {
+      const response = await this.openAiApi.createCompletion({
+        model: "text-davinci-003",
+        prompt: this.generatePrompt(text),
+        temperature: 0.6,
+      });
+      this.response = response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  generatePrompt(text: string) {
+    return `The text I want to complete is: ${text}`;
+  }
+
+  submitForm() {
+    this.doSomething(this.text);
+  }
+
 }
